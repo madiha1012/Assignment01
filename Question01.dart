5fdfd6a7235751029ee78f3b32f1336d8d936db2
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-09-28 22:06:58 +0500

    Add files via upload
    Question 01

---
+++ Question01.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Question 01");
+  
+  int length = 5;
+ print("length is $length");
+  int breadth = 7;
+ print("breadth is $breadth");
+  print("==============");
+ print("area of rectangle ${length * breadth}");
+}
